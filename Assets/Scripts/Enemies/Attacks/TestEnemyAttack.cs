12a691465f172e8e5f4c917d5c2a42abd6bd6063
Maayana24 <maayan2412001@gmail.com>
2025-05-20 00:44:40 +0300

    Target Lowest health player

--- Assets/Scripts/Enemies/Attacks/TestEnemyAttack.cs
+++ Assets/Scripts/Enemies/Attacks/TestEnemyAttack.cs
@@ -1,17 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TestEnemyAttack : EnemyAttack
 {
     private const string Player = "Player";
-    private PlayerCombatManager combatManager;
+    private PlayerCombatManager playerToAttack;
 
-    protected override bool IsPlayerInRange => combatManager != null;
+    private List<PlayerCombatManager> players;
+
+    protected override bool IsPlayerInRange => playerToAttack;
     protected override void ApplyDamage()
     {
-        if (combatManager)
-        {
-            combatManager.TakeDamage(new DamageArgs { Damage = manager.EnemyData.BaseDamage });
-        }
+        playerToAttack.TakeDamage(new DamageArgs { Damage = manager.EnemyData.BaseDamage });
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -19,10 +19,31 @@ public class TestEnemyAttack : EnemyAttack
         if (collision.CompareTag(Player))
         {
             Debug.Log("Player Found");
-            combatManager = collision.gameObject.GetComponent<PlayerCombatManager>();
-            if (combatManager)
+            players.Add(collision.gameObject.GetComponent<PlayerCombatManager>());
+            if (players.Count > 1)
             {
                 Debug.Log("Combat manager found");
+                for(int i = 0; i <= players.Count; i++)
+                {
+                    for (int j = i + 1; j <= players.Count; j++)
+                    {
+                        if (players[i].CurrentHealth > players[j].CurrentHealth)
+                        {
+                            players[i] = playerToAttack;
+                        }
+                        else
+                        {
+                            players[j] = playerToAttack;
+                        }
+                    }
+                }
+            }
+            else if (players.Count == 1)
+            {
+                players[0] = playerToAttack;
+            }
+            if(playerToAttack)
+            {
                 TryAttack();
             }
         }
@@ -33,7 +54,11 @@ public class TestEnemyAttack : EnemyAttack
         if (collision.CompareTag(Player))
         {
             Debug.Log("Player left attack range");
-            combatManager = null;
+            players.Remove(collision.gameObject.GetComponent<PlayerCombatManager>());
+            if(players.Count <= 0)
+            {
+                playerToAttack = null;
+            }
         }
     }
 }
