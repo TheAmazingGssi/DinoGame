dfce3699e09563852faae7b54ab68a8d949aeb61
Maayana24 <maayan2412001@gmail.com>
2025-06-18 17:48:17 +0300

    VotingManagerWIP

--- Assets/Scripts/UI/Voting/Vote.cs
+++ Assets/Scripts/UI/Voting/Vote.cs
@@ -1,15 +1,15 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class Vote
-{
-    public string voteDescription { get; private set; }
-    public string choice1 { get; private set; }
-    public string choice2 { get; private set; }
+//public class Vote
+//{
+//    public string voteDescription { get; private set; }
+//    public string choice1 { get; private set; }
+//    public string choice2 { get; private set; }
 
-    public Vote(string voteDescription, string choice1, string choice2 )
-    {
-        this.voteDescription = voteDescription;
-        this.choice1 = choice1;
-        this.choice2 = choice2;
-    }
-}
+//    public Vote(string voteDescription, string choice1, string choice2 )
+//    {
+//        this.voteDescription = voteDescription;
+//        this.choice1 = choice1;
+//        this.choice2 = choice2;
+//    }
+//}
