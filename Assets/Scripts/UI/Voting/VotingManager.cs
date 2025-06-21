394b1463f7bc1c798bce107445c5db0bfcdbfe9c
Maayana24 <maayan2412001@gmail.com>
2025-06-21 16:53:17 +0300

    mbvhnhv

--- Assets/Scripts/UI/Voting/VotingManager.cs
+++ Assets/Scripts/UI/Voting/VotingManager.cs
@@ -1,12 +1,8 @@
-using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-using static System.Net.Mime.MediaTypeNames;
-
 public class VotingSystem : MonoBehaviour
 {
     [Header("UI References")]
