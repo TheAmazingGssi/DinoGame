8ae6e2c8a9ded2c576190f5876fe4fc7e49cb206
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2025-07-05 16:04:44 +0300

    New main screen buttons

--- Assets/Scripts/Misc and Utility/ParalaxAnimation.cs
+++ Assets/Scripts/Misc and Utility/ParalaxAnimation.cs
@@ -1,34 +1,30 @@
 using UnityEngine;
 
-public class Paralax : MonoBehaviour
+public class ParalaxAnimation : MonoBehaviour
 {
-    [SerializeField] Camera mainCamera;
+    float focalPoint;
     [SerializeField] ParalaxLayer[] layers;
+    [SerializeField] float speed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         for (int i = 0; i < layers.Length; i++)
         {
-            layers[i].CameraOffset = mainCamera.transform.position.x - layers[i].LayerObject.transform.position.x;
+            layers[i].CameraOffset = focalPoint - layers[i].LayerObject.transform.position.x;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        focalPoint += speed * Time.deltaTime;
+
         foreach (ParalaxLayer layer in layers)
         {
-            float layerX = (mainCamera.transform.position.x * layer.Multiplier) - layer.CameraOffset;
+            float layerX = (focalPoint * layer.Multiplier) - layer.CameraOffset;
+            layerX = layerX % layer.sprite.sprite.
             layer.LayerObject.transform.position = new Vector2(layerX, layer.LayerObject.transform.position.y); //x changes, y stays the same
         }
     }
-}
-[System.Serializable]
-struct ParalaxLayer
-{
-    public GameObject LayerObject;
-    public float Multiplier;
-    public SpriteRenderer sprite;
-    [HideInInspector]public float CameraOffset;
 }
