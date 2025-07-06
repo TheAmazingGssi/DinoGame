ff564acb600812f47af746fc1d59236586d650d5
Maayana24 <maayan2412001@gmail.com>
2025-07-06 20:22:50 +0300

    Vote UI Design

--- Assets/Scripts/Voting/ConcreteVotes/HelpSaberToothVSStealAcorn.cs
+++ Assets/Scripts/Voting/ConcreteVotes/HelpSaberToothVSStealAcorn.cs
@@ -6,7 +6,10 @@ public class HelpSaberToothVSStealAcorn : Vote
 {
     public override int LevelNumber => GameManager.Instance.LevelNumber;
     public override string VoteTitle => "Help the saber tooth squirrel\r\nVS.\r\nSteal his acorn";
-    public override string VoteDescription => "Help the saber tooth squirrel\r\nVS.\r\nSteal his acorn";
+    public override string VoteDescription => "As you pass the vicious western desert," +
+        " you all feel a chill running down your spine, [most score dino] can't shake the feeling you’re being watched," +
+        " and soon after you find Erl the sabertooth squirrel, he warns you that a blizzard is on its way! He asks you to shelter" +
+        " his acorns and promises a great reward if you agree. Or.. you can always steal them for extra nutrition";
 
     public override string[] Choices => new string[2] { "Shelter the acorn\r\ntake 20% less damage.\n<color=red>BUT</color>\nMore ice meteors on the next level.\r\n",
         "Steal the acorns\r\nraise max health\n<color=red>BUT</color>\nthe highest score player takes more damage next level."};
