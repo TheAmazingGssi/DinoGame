d869b8147c266b30ba193a86a4c04d3cce11a1f7
Maayana24 <maayan2412001@gmail.com>
2025-07-12 16:11:15 +0300

    how to play done

--- Assets/Scripts/UI/Menu/HowToPlayMenuManager.cs
+++ Assets/Scripts/UI/Menu/HowToPlayMenuManager.cs
@@ -1,9 +1,15 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HowToPlayMenuManager : MonoBehaviour
 {
+    [SerializeField] private Button backButton;
+    [SerializeField] private Button continueButton;
     [SerializeField] private Button howToPlayButton;
+
+    [SerializeField] private TextMeshProUGUI controlsText;
+    [SerializeField] private TextMeshProUGUI GameText;
 
     [Header("Prefab Refrences")]
     [SerializeField] Selectable defaultSelection;
@@ -17,5 +23,22 @@ public class HowToPlayMenuManager : MonoBehaviour
     {
         gameObject.SetActive(false);
         howToPlayButton.Select();
+    }
+
+    public void Continue()
+    {
+        controlsText.gameObject.SetActive(false);
+        GameText.gameObject.SetActive(true);
+        backButton.gameObject.SetActive(true);
+        continueButton.gameObject.SetActive(false);
+        backButton.Select();
+    }
+    public void Back()
+    {
+        GameText.gameObject.SetActive(false);
+        controlsText.gameObject.SetActive(true);
+        backButton.gameObject.SetActive(false);
+        continueButton.gameObject.SetActive(true);
+        continueButton.Select();
     }
 }
