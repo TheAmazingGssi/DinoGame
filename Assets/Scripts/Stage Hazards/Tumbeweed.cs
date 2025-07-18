a4fb7b11aec420511daba035e7a1b5b42fe23467
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2025-07-18 12:50:35 +0300

    work on level 2 + fixing level 1

---
+++ Assets/Scripts/Stage Hazards/Tumbeweed.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Tumbeweed : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
