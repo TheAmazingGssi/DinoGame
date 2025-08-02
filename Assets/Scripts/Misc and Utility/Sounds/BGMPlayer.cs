7d22f9e0f2aefade292690e433be48642f4ee39e
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2025-08-02 17:28:13 +0300

    new sounds a

--- Assets/Scripts/Misc and Utility/Sounds/BGMPlayer.cs
+++ Assets/Scripts/Misc and Utility/Sounds/BGMPlayer.cs
@@ -10,11 +10,21 @@ public class BGMPlayer : MonoBehaviour
     void Start()
     {
         //this is an anti singleton, if a new one gets made it deletes the old instance instead of keeping the original instance.
-        if(instance)
+        //its made this way to change songs by going into a scene that should have a new song
+        //however if its meant to keep playing the same song it will delete the new one instead, like a normal singleton
+        if (instance)
+        {
+            if(music == instance.music)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
             Destroy(instance.gameObject);
+        }
+
         instance = this;
         DontDestroyOnLoad(gameObject);
-
         soundPlayer.PlaySound(music);
     }
 }
