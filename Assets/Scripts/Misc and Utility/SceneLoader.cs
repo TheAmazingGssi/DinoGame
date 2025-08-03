83e7c74b86f619a47f7b180a251a58d8ed044f68
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2025-08-03 13:44:48 +0300

    build bugfixing

--- Assets/Scripts/Misc and Utility/SceneLoader.cs
+++ Assets/Scripts/Misc and Utility/SceneLoader.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum Scenes { MainMenu = 0, CharacterSelect = 1, Level1 = 2, CharacterSelectToUITesting = 3, EdenTestingScene = 4, UITestingScene = 5, Level2 = 6, Level3 = 7};
+public enum Scenes { MainMenu = 0, CharacterSelect = 1, Level1 = 2, Level2 = 3, Level3 = 4};
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] Scenes targetScene;
