6be89bae6e7359817e15110b8a259ee827e7c0fc
Milo413 <lotemgad@gmail.com>
2025-09-04 15:18:08 +0300

    Not sure whats this

---
+++ Assets/Scripts/Player/CharactersSpecifics/SpencerHeadLocationTracking.cs
@@ -0,0 +1,14 @@
+/*using System;
+using UnityEngine;
+
+public class SpencerHeadLocationTracking : MonoBehaviour
+{
+    [SerializeField] Transform headTransform;
+    [SerializeField] GameObject NeckEdge;
+
+    private void Update()
+    {
+        headTransform = NeckEdge.transform;
+    }
+}
+*/
---
+++ Assets/Scripts/Player/Tests/PlayerCombatTest.cs
@@ -0,0 +1,63 @@
+/*using UnityEngine;
+
+public class PlayerCombatManager : CombatManager
+{
+    public void Initialize(float maxHealth)
+    {
+        currentMaxHealth = maxHealth;
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    public override void TakeDamage(DamageArgs damageArgs) //gets called from the enemy
+    {
+        base.TakeDamage(damageArgs);
+        Debug.Log("Player took Damage");
+    }
+
+
+}
+
+using UnityEngine;
+
+public class PlayerCombatTest : CombatManager
+{
+    private static readonly int Hurt = Animator.StringToHash("Hurt");
+    private static readonly int Fallen = Animator.StringToHash("Fallen");
+    [SerializeField] private PlayerTransformData playerTransform;
+
+
+    [SerializeField] private TextMesh damageNumberPrefab;
+
+    public void Initialize(float maxHealth)
+    {
+        playerTransform.PlayerTransform = transform;
+        currentMaxHealth = maxHealth;
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    public override void TakeDamage(DamageArgs damageArgs)
+    {
+       // if (currentHealth <= 0) return;
+        base.TakeDamage(damageArgs);
+        Debug.Log("Player took: " + damageArgs);
+        if (damageNumberPrefab)
+        {
+            TextMesh damagePrefabClone = Instantiate(damageNumberPrefab, transform.position, Quaternion.identity, transform);
+            damagePrefabClone.text = damageArgs.Damage.ToString();
+        }
+    }
+
+    protected override void HandleDeath()
+    {
+        base.HandleDeath(); // Invokes OnDeath event
+    }
+
+    public void Revive(float healthFraction)
+    {
+        currentHealth = healthFraction * currentMaxHealth;
+        UpdateHealthBar();
+    }
+}
+*/
